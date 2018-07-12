e89e7b27428a9ba602e0ddacc07405057fe2f61f
DianaLam06 <diana.lam06@gmail.com>
2018-07-12 05:30:17 +0000

    simplified template and app

--- app.py
+++ app.py
@@ -14,40 +14,10 @@ except ImportError:
         return dec
 
 import flask
-from bokeh.embed import components
-from bokeh.plotting import figure
-from bokeh.resources import INLINE
-from bokeh.util.string import encode_utf8
 import pandas as pd
 from os.path import dirname, join
-from bokeh.io import curdoc
-from bokeh.layouts import row, column
-from bokeh.models import ColumnDataSource
-from bokeh.models.widgets import PreText, Select
 
 app = flask.Flask(__name__)
-
-colors = {
-    'Black': '#000000',
-    'Red':   '#FF0000',
-    'Green': '#00FF00',
-    'Blue':  '#0000FF',
-}
-
-months = {
-    'Jan' : 1,
-    'Feb' : 2,
-    'Mar' : 3,
-    'Apr' : 4,
-    'May' : 5,
-    'Jun' : 6,
-    'Jul' : 7,
-    'Aug' : 8,
-    'Sep' : 9,
-    'Oct' :10,
-    'Nov' :11,
-    'Dec' :12
-    }
 
 def getitem(obj, item, default):
     if item not in obj:
@@ -55,14 +25,8 @@ def getitem(obj, item, default):
     else:
         return obj[item]
 
-DATA_DIR = join(dirname(__file__), 'daily')
 
 @lru_cache()
-def load_ticker(ticker):
-    fname = join(DATA_DIR, 'table_%s.csv' % ticker.lower())
-    data = pd.read_csv(fname, header=None, parse_dates=['Date'],
-                       names=['Date', 'foo', 'o', 'h', 'l', 'Close', 'v'])
-    return data[['Date','Close']]
 
 
 @app.route('/')
@@ -72,26 +36,15 @@ def index():
     args = flask.request.args
 
     # Get all the form arguments in the url with defaults
-    color = getitem(args, 'color', 'Black')
-    ticker = getitem(args, 'stock', 'AAPL')
-    month = getitem(args, 'month', 'Jan')
+    inputted_string = getitem(args, 'movie_name', 'Annihilation')
 
-    stockdata_all = load_ticker(ticker)
-    stockdata = stockdata_all[(stockdata_all['Date'].dt.month==months[month]) & (stockdata_all['Date'].dt.year==2000)]
-    fig = figure(title=ticker.upper()+" data for 2000", x_axis_type = "datetime")
-    fig.line(stockdata['Date'], stockdata['Close'], color=colors[color], line_width=2)
 
-    resources = INLINE.render()
-
-    script, div = components(fig)
+    # resources = INLINE.render()
+    # script, div = components(fig)
+    
     html = flask.render_template(
         'embed.html',
-        plot_script=script,
-        plot_div=div,
-        resources=resources,
-        color=color,
-        ticker=ticker,
-        month=month
+        movie_string=inputted_string
     )
     
     return html
