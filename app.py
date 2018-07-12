6b5bf9a1d35df3970803c4d161056e05756ce465
DianaLam06 <diana.lam06@gmail.com>
2018-07-12 22:54:31 +0000

    add rating, genre, production

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import flask
 import pandas as pd
 from os.path import dirname, join
 import requests
-#from io import BytesIO
+from io import BytesIO
 
 app = flask.Flask(__name__)
 
@@ -105,20 +105,20 @@ def getAPIdata(string):
     id_num_ = getMovieID(response)
     details = getAPIwID(id_num_)
     
-    #rating = getRating(details)
-    #genre = getGenre(details)
-    #production = getProduction(details)
+    rating = getRating(details)
+    genre = getGenre(details)
+    production = getProduction(details)
     #poster = getPoster(response)
     
-    #all_details = {}
+    all_details = {}
     
-    #all_details['rating'] = rating
-    #all_details['genre'] = genre
+    all_details['rating'] = rating
+    all_details['genre'] = genre
     #all_details['poster'] = poster
-    #all_details['production'] = production
+    all_details['production'] = production
     
-    return "test me"
     #return details
+    return all_details
     #return response
 
 @lru_cache()
