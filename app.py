fabb474f714adb54badf342f43f53b79a4ec537f
DianaLam06 <diana.lam06@gmail.com>
2018-08-24 06:37:47 +0000

    getting figures

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from make_figures import *
+from get_TMDB import * 
 # need to figure out why atonement, the maze runner, and divergent are not working
 # may be multiple responses from first requests.get?
 
@@ -23,7 +24,9 @@ import requests
 from io import BytesIO
 from PIL import Image
 import datetime
-
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import StringIO
 
 app = flask.Flask(__name__)
 
@@ -33,151 +36,25 @@ def getitem(obj, item, default):
     else:
         return obj[item]
 
-def getMovieID(response):
-    '''
-    input: response from requests.get, used to get numeric id of movie
-    output: MovieID key (int)
-    '''
-    movie_json = response.json()
-    return movie_json['results'][0]['id']
-
-
-
-# In[12]:
-
-def getAPIwID(id_num):
-    '''
-    input: the id number (int) of the move to read into the API to get details
-    output: a json with movie details
-    
-    response_full = requests.get("https://api.themoviedb.org/3/movie/" + str(id_num) + \
-                            "?api_key=900a489ed1a09a120f925244bffb3f34&language=en-US&append_to_response=details,keywords,release_dates")
-    '''
-    response_full = requests.get("https://api.themoviedb.org/3/movie/" + str(id_num) +                             "?api_key=900a489ed1a09a120f925244bffb3f34&language=en-US&append_to_response=details,keywords,videos,release_dates")
-    
-    return response_full.json()
-
-def getRating(deets):
-    '''
-    input: deets, a json containing the movie details
-    output: dictionary with certification
-    '''
-    try:
-        if 'release_dates' in deets.keys():
-            for item in deets['release_dates']['results']:
-                if item.values()[0] == u'US':
-                    for subitem in item.values()[1]:
-                        if subitem['certification'] != u'':
-                            return subitem['certification']
-    except AttributeError:
-        return 'No rating found'
-    
-
-
-def getGenre(deets):
-    '''
-    input: deets, a json containing movie details
-    output: single string with all genres, separated by commas
-    ''' 
-    genre_list = [ i['name'] for i in deets['genres']]
-    genre_string = ", ".join(genre_list)
-    
-    return genre_string
-    
-    
-
-
-# In[9]:
-
-
-def getProduction(deets):
-    '''
-    input: deets, a json containing movie details from ID grab
-    output: the production companies involved in a string, separated by commas
-    '''
-    return ", ".join([ i['name'] for i in deets['production_companies']])
-
-    
-
-def getPoster(resp):
-    '''
-    input: the first API response from search
-    output: picture of the movie poster
-    '''
-    poster_path = resp.json()['results'][0]['poster_path']
-    full_path = u"https://image.tmdb.org/t/p/w342/" + poster_path    
-    URL = requests.get(full_path)
-    img = Image.open(BytesIO(URL.content))
-    return full_path
-
-def getReleaseDateWithEmpty(deets):
-    '''
-    input: json with movie details
-    otuput: release date
-    '''
-    if 'release_dates' in deets.keys() and 'results' in deets['release_dates'].keys():
-        for item in deets['release_dates']['results']:
-            try:
-                if item.values()[0] == u'US':
-                    for subitem in item.values()[1]:
-                        if subitem['certification'] != u'':
-
-                            s = item.values()[1][0]['release_date']
-
-                            s_short = re.search(u'\d{4}-\d{2}-\d{2}', s).group()
-                            t = datetime.datetime.strptime(s_short, u"%Y-%m-%d")
-                            return t
-            except AttributeError:
-                return 'No release date found'
-
-
-def getAPIdata(string):
-    '''
-    input: a string, which is a movie name, if input is xxyyzz then output blanks with a non commital picture for poster url
-    output: movie details from API in a dict
-    '''
-    if string == " ":
-        rating = " "
-        genre_html = " "
-        production = " "
-        poster = 'http://4.bp.blogspot.com/--vVGyhWo610/VLcuSaQZROI/AAAAAAAAADg/6mYoDt05hJg/s1600/booksvsmovies.jpg'
-        wait_time = " "
-       
-     
-    else:
-
-        string_url = "+".join(string.split())
-        response = requests.get("https://api.themoviedb.org/3/search/movie?api_key=900a489ed1a09a120f925244bffb3f34&query=" + string_url)
-
-        id_num_ = getMovieID(response)
-        details = getAPIwID(id_num_)
-
-        rating = getRating(details)
-        genre_html = getGenre(details)
-        production = getProduction(details)
-        poster = getPoster(response)
-        release_date = getReleaseDateWithEmpty(details)
-        
-        
-    if string.lower() == "crazy rich asians":
-        rating = "PG-13"
-        production = "Color Force, SK Global, Warner Bros."
-        
-
-
-    all_details = {}
-    
-    all_details['rating'] = rating
-    all_details['genre'] = genre_html
-    all_details['poster'] = poster
-    all_details['production'] = production
-    all_details['release_date'] = release_date
-  
-    return all_details
 
 
 @lru_cache()
 
+@app.route('/plot.png')
+def plot():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+
+    axis.plot(xs, ys)
+    canvas = FigureCanvas(fig)
+    output = StringIO.StringIO()
+    canvas.print_png(output)
+    response = make_response(output.getvalue())
+    response.mimetype = 'image/png'
+    return response
 
 @app.route('/')
 def index():
