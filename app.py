a69eb8fc08e44be252f4df2870603789e5b743d7
DianaLam06 <diana.lam06@gmail.com>
2018-08-24 09:34:55 +0000

    printing release_date

--- app.py
+++ app.py
@@ -73,9 +73,9 @@ def index():
     cluster_indiv = km4.predict(X_indiv).item(0)
     predict_indiv = logistic_genre_cert_PCA.predict(X_indiv).item(0)
     
+    result_dict['wait_time'] = result_dict['release_date']
     
-    
-    
+    '''
     if result_dict['release_date'].year - datetime.datetime.today().year > 2 :
         result_dict['wait_time'] = 'Movie was released more than 2 years ago, demand should be reasonable unless book is part of a series'
     elif predict_indiv == 0:
@@ -84,7 +84,7 @@ def index():
         result_dict['wait_time'] = 'Buy, expected wait longer than a year'
     else :
         result_dict['wait_time'] = 'No prediction, missing feature information'
-    
+   ''' 
 
 
 
